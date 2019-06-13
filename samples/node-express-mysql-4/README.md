738232db19857a0f214cb275dee20a37bb001b00
Fabrício Silva <fabriciofmsilva@gmail.com>
2019-06-13 13:32:03 -0300

    Creating a Role-Based User Authentication System with Angular, Express and MySQL

---
+++ samples/node-express-mysql-4/README.md
@@ -0,0 +1,11 @@
+# Creating a Role-Based User Authentication System with Angular, Express and MySQL 
+
+Creating role-based user authentication system using Express, MySQL, Sequelize, Passport and JSON Web Tokens (JWT) on the backend and use Angular 1.x to drive our application’s front-end.
+
+* Allows users to sign-up and login
+* Supports multiple user roles (Guest, User, Admin)
+* Restricts route access based on user role
+
+## Resources
+
+* [Creating a Role-Based User Authentication System with Angular, Express and MySQL](https://catalin.me/role-based-authentication-with-angular-express-jwt-mysql-part-1/)
---
+++ samples/node-express-mysql-4/app/config.js
@@ -0,0 +1,32 @@
+// Application configuration.
+'use strict';
+
+var config = module.exports;
+
+config.db = {
+    user: 'root', 
+    password: 'root',
+    name: 'auth'
+};
+
+config.db.details = {
+    host: process.env.MYSQL_HOST || 'localhost',
+    port: 3306,      
+    dialect: 'mysql'
+};
+
+config.keys = {
+    secret: '/jVdfUX+u/Kn3qPY4+ahjwQgyV5UhkM5cdh1i2xhozE=' // Not anymore...
+};
+
+var userRoles = config.userRoles = {
+    guest: 1,    // ...001
+    user: 2,     // ...010
+    admin: 4     // ...100
+};
+
+config.accessLevels = {
+    guest: userRoles.guest | userRoles.user | userRoles.admin,    // ...111
+    user: userRoles.user | userRoles.admin,                       // ...110
+    admin: userRoles.admin                                        // ...100
+};
